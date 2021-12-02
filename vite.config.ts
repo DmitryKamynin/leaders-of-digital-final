9129fd7ec916c323eb600e058755bb788c48197c
Dmitry Kamynin <d.kamynin>
2021-12-02 18:31:06 +0300

    удалил сервер удалил статы поправил конфиг сборщика

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import reactSvgPlugin from "vite-plugin-react-svg";
 import * as path from "path";
-import { visualizer } from "rollup-plugin-visualizer";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      plugins: [
-        visualizer({
-          filename: "stasts.html",
-          gzipSize: true,
-        }),
-      ],
+      plugins: [],
     },
   },
   server: {
